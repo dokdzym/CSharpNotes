d83efdbfe19ad5bdb98257669b96b944ec390cc6
dokdzym <812746443@qq.com>
2020-10-12 11:20:18 +0800

    Array;Delegate

---
+++ Array.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Array
+{
+    class Array
+    {
+        static void Main(string[] args)
+        {
+            //以下三种方式等价
+            int[,] b = new int[,]   { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+            int[,] c = new int[3,2] { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+            int[,] d =              { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+            var    e = new [,]       { { 1, 2 }, { 3, 4 }, { 5, 6 } };  //隐式类型
+
+            //******************以上是矩形数组的语法************************//
+
+
+            //******************以下是交错数组的语法************************//
+
+            //如何声明交错数组
+            //int[][] f = new int[3][4];//编译错误，只能声明顶层数组的维数
+            int[][] f = new int[3][];//正确
+            int[][,] arr;
+            arr = new int[3][,];//维度为3，数组元素是一个二维的矩形数组
+
+            //以下对交错数组arr的三个元素进行赋值，这三个元素的类型是int[,]，即二维矩形数组
+            //arr的三个元素都是指向另外三个数组的引用，在赋值完成后，一共存在四个数组
+            arr[0] = new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+            arr[1] = new int[,] { { 7, 8 }, { 9, 10 }, { 11, 12 } };
+            arr[2] = new int[,] { { 13, 14 }, { 15, 16 }, { 17, 18 } };
+
+            //数组都从System.Array类继承而来，以下使用一些常用方法
+            Console.WriteLine("Rank of arr = {0}", arr.Rank);
+            Console.WriteLine("Rank of arr[0] = {0}", arr[0].Rank);
+            Console.WriteLine("arr.GetLength(0) = {0}", arr.GetLength(0)); //获取arr第一维的长度
+            //Console.WriteLine("arr.GetLength(1) = {0}", arr.GetLength(1));//运行错误，试图获取arr第二维的长度，但是arr只有一个维度
+
+            int[,] tmpArr = (int[,])arr.GetValue(2);//取index为2的这一维
+            int[,] tmpArr2 = arr[2];                //与上一行等价         
+
+            
+            System.Array.Reverse(arr);
+            arr[2] = (int[,]) arr[1].Clone(); //浅复制
+
+            //遍历数组arr
+            for (int i = 0; i < arr.GetLength(0); ++i)
+            {
+                for (int j = 0; j < arr[i].GetLength(0); ++j)
+                {
+                    for (int k = 0; k < arr[i].GetLength(1); ++k)
+                    {
+                        Console.WriteLine("arr[{0}][{1},{2}] = {3}", i, j, k, arr[i][j, k]);
+                    }
+                    Console.WriteLine("");
+                }
+                Console.WriteLine("");
+            }
+
+            foreach(int[,] i in arr)
+            {
+                foreach(int tmp in i)
+                {
+                    //tmp = tmp;//tmp是数组i的只读别名，无法赋值
+                    //如果数组里存的是class对象，那么可以通过这个引用改变对象里的值
+                    Console.WriteLine(tmp);
+                }
+            }
+            //******************以上是交错数组的语法************************//
+            //在CIL中，一维数组有特定的指令用于性能优化，因此交错数组比起矩形数组效率更高
+        }
+    }
+}
---
+++ Delegate.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegate
+{
+    delegate void MyDel();
+    delegate int MyDelWithReturn(int x);
+    class Delegate
+    {
+        static void func1()
+        {
+            Console.WriteLine("func1 called");
+        }
+        void func2()
+        {
+            Console.WriteLine("func2 called");
+        }
+        static void Main(string[] args)
+        {
+            MyDel delInstance;
+            //delInstance += Delegate.func1;               //编译错误，必须先给委托对象赋值
+            delInstance = Delegate.func1;                  //赋值一个static成员函数
+            delInstance = new MyDel(Delegate.func1);       //与上一句等价
+
+            Delegate obj = new Delegate();
+            delInstance -= Delegate.func1;                 //删除委托对象里的func1函数
+            Console.WriteLine("******************************");
+            //delInstance();                               //抛出异常，因为试图调用空委托对象
+            if(null == delInstance)
+            {
+                Console.WriteLine("委托对象的调用列表为空");//一般都会先做null检查，再去调用委托对象
+            }
+
+
+            delInstance -= obj.func2;                      //试图删除委托对象里不存在的函数，并不会报错
+
+            delInstance += obj.func2;                      //添加一个对象成员函数    
+
+            Console.WriteLine("******************************");
+            delInstance();
+
+            //委托对象可以返回值
+            //委托对象的ref参数将传递给调用列表的下一个函数
+
+
+            /********************************************/
+            //使用匿名方法初始化一个带返回值的委托对象
+            //格式为delegate(参数列表){函数体};
+            MyDelWithReturn delInstance2;
+            {
+                int y = 10;
+                delInstance2 = delegate (int x)
+                {
+                    //return x + 10;         
+                    return x + y;          //与上一行等价，因为匿名方法可以捕获外部变量
+                };                         //记得加分号，这是一个语句！
+
+                //delInstance2 += (int x) => { return x + y + 10; };  //正常运行，lambda表达式，=>读作"goes to"
+            }
+            //Console.WriteLine("y = {0}", y);     //编译错误，局部变量y已过期
+            Console.WriteLine("x + y = {0}", delInstance2(3)); //y已被委托对象捕获，程序正常运行
+
+            //delegate后的圆括号可以省略，但必须满足：不使用任何参数
+            //可以省略参数类型
+            //如果只有一个参数，可以省略圆括号()
+            //如果函数体只有一行返回语句，可以省略花括号{}
+        }
+    }
+}
